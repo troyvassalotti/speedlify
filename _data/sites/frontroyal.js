5b514f68e0f11f2e1f75121975346fdf141de254
Troy Vassalotti <45142012+troyvassalotti@users.noreply.github.com>
2021-06-20 08:53:55 -0400

    stop testing front royal

--- _data/sites/frontroyal.js
+++ _data/sites/frontroyal.js
@@ -1,6 +1,7 @@
 module.exports = {
     name: "frontroyalband.com", // optional, falls back to object key
     description: "The Band, Not The Town",
+    skip: true,
     options: {
         frequency: 60 * 23, // (in minutes), 23 hours
     },
