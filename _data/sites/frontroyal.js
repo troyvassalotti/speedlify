562bf1f5586a81f225eba6ed091907abe547fece
Troy Vassalotti <45142012+troyvassalotti@users.noreply.github.com>
2021-07-23 21:56:24 -0400

    pause Fr, again

--- _data/sites/frontroyal.js
+++ _data/sites/frontroyal.js
@@ -1,6 +1,7 @@
 module.exports = {
     name: "frontroyalband.com", // optional, falls back to object key
     description: "Front Royal",
+    skip: true, // I don't know why this is taking so long to build each time, but the scores aren't that bad...
     options: {
         frequency: 60 * 23, // (in minutes), 23 hours
     },
