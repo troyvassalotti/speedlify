041d5d8e88bf33613c30f6f47de388c0f86d18fe
Troy Vassalotti <45142012+troyvassalotti@users.noreply.github.com>
2021-10-02 16:36:21 -0400

    once again, remove front royal

--- _data/sites/frontroyal.js
+++ _data/sites/frontroyal.js
@@ -1,11 +1,11 @@
 module.exports = {
     name: "frontroyalband.com", // optional, falls back to object key
     description: "Front Royal",
-    // skip: true,
+    skip: true,
     options: {
         frequency: 60 * 23, // (in minutes), 23 hours
     },
     urls: [
-        "https://www.frontroyalband.com/"
+        "https://www.frontroyalband.com/about"
     ]
 };
